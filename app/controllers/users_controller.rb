7c1b9cdc1aa2c000d24c900092d5dc52a5727ea8
mik3ymikes <mikey.henleys@gmail.com>
2024-01-30 17:04:29 -0600

    got to update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+    def index
+      @users = User.all
+      render json: @users
+    end
+  
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+  
     def create
-      user = User.new(name: params[:name], email: params[:email])
-      if user.save
-        render json: user
+      @user = User.new(name: params[:name], email: params[:email])
+      if @user.save
+        render json: @user
       else
         render json: { error: "Unable to create user." }
+      end
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(name: params[:name], email: params[:email])
+        render json: @user
+      else
+        render json: { error: "Unable to update user." }
       end
     end
   end
